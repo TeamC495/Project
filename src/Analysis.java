e5972c66e6d3295bf6008162b74cdbc93cb1f784
bnorman7 <bnorman7@users.noreply.github.com>
2014-04-22 20:46:25 -0400

    Update Analysis.java

--- src/Analysis.java
+++ src/Analysis.java
@@ -1,5 +1,33 @@
+
 package teamC;
 
 public class Analysis {
+	
 
-}
+	// variables
+	int originalSize;
+	int receivedSize;
+	float ratio;
+	
+	// constructor
+	Analysis (int receivedSize, int originalSize){
+		
+	this.receivedSize = receivedSize;
+	this.originalSize = originalSize;
+	ratioCalculator();
+	
+	}
+	// determine ratio
+	void ratioCalculator(){
+		
+	ratio = ((float)(receivedSize))/((float)(originalSize));
+	
+	}
+	
+	// get ratio as a percentage
+	int getRatio (){
+	
+	return (int)(ratio * 100);
+	
+	}}
+
