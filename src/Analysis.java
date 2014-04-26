56d7ba85f9cfeb6984405daebe81bb6d7a72a845
bnorman7 <bnorman7@users.noreply.github.com>
2014-04-26 16:54:45 -0400

    Updated header comments Analysis.java

--- src/Analysis.java
+++ src/Analysis.java
@@ -1,3 +1,25 @@
+/** 
+ * Class: CMSC495 Spring 2014
+ * Assignment: Capstone, Week 6, Sprint 1
+ * 
+ * Team Members: Brad Norman, Jamie Lane, Daniel Ross
+ * 
+ * Date: 26 April 2014 
+ * 
+ * File:	RdosTester.java (main application) 	
+ * 			Packet.java
+ * 			PacketTransmitter.java
+ * 			->Analysis.java<-
+ * 			
+ * 			Demonstrate a reflected and amplified Denial-of-Service attack mechanism
+ * 			exploiting a vulnerability in Open Arena server versions <= 0.8.5
+ * 
+ * Current Status: Calculates packet size ratio (received packet size/original packet size)
+ * 
+ * @author Brad Norman
+ *           
+ **/
+
 package teamC;
 
 public class Analysis {
