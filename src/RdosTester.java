2df9ff16ef5de67c9b2f6a34a70edfdc135c5e90
bnorman7 <bnorman1088@gmail.com>
2014-04-30 12:16:09 -0400

    Added input validation and default values

--- src/RdosTester.java
+++ src/RdosTester.java
@@ -57,6 +57,15 @@ public class RdosTester extends JPanel implements ActionListener
     private String message;
     private JPanel panel;
     private JPanel spacer;
+    private int validSrcIP1;
+    private int validSrcIP2;
+    private int validSrcIP3;
+    private int validSrcIP4;
+    private int validDstIP1;
+    private int validDstIP2;
+    private int validDstIP3;
+    private int validDstIP4;
+    private int validPort;
 
     // Constructor initializes gui components and layout.
     public RdosTester() 
@@ -64,12 +73,16 @@ public class RdosTester extends JPanel implements ActionListener
     	// Initialize the source IP address labels and textfields
     	sourceLabel = new JLabel("Source IP Address:");
     	srcIP1 = new JTextField(3);
+        srcIP1.setText("127");
     	srcIP1.addActionListener(this);
     	srcIP2 = new JTextField(3);
+        srcIP2.setText("0");
     	srcIP2.addActionListener(this);
     	srcIP3 = new JTextField(3);
+        srcIP3.setText("0");
     	srcIP3.addActionListener(this);
     	srcIP4 = new JTextField(3);
+        srcIP4.setText("1");
     	srcIP4.addActionListener(this);
     	dot1 = new JLabel(DOT);
     	dot2 = new JLabel(DOT);
@@ -78,12 +91,16 @@ public class RdosTester extends JPanel implements ActionListener
     	// Initialize the destination IP address labels and textfields
     	destinationLabel = new JLabel("Destination IP Address:");
     	dstIP1 = new JTextField(3);
+        dstIP1.setText("127");
     	dstIP1.addActionListener(this);
     	dstIP2 = new JTextField(3);
+        dstIP2.setText("0");
     	dstIP2.addActionListener(this);
     	dstIP3 = new JTextField(3);
+        dstIP3.setText("0");
     	dstIP3.addActionListener(this);
     	dstIP4 = new JTextField(3);
+        dstIP4.setText("1");
     	dstIP4.addActionListener(this);
     	dot4 = new JLabel(DOT);
     	dot5 = new JLabel(DOT);
@@ -92,6 +109,7 @@ public class RdosTester extends JPanel implements ActionListener
     	// Initialize the port label and textfield
     	portLabel = new JLabel("Port:");    	
     	port = new JTextField(6);
+        port.setText("27960");
     	port.addActionListener(this);
     	
     	// Initialize the button
@@ -146,35 +164,116 @@ public class RdosTester extends JPanel implements ActionListener
      * the Transmit button is the only component with an action
      * tied to it. 
      **/
+    
+        
     public void actionPerformed(ActionEvent e) 
     {
+              
     	if(e.getSource() == button)
-    	{	    	
-    		// Create the original packet. In future iterations, the textfields will already hold
-    		// integer values and will not have to convert them from strings.
-	    	RdosPacket originalPacket = new RdosPacket(Integer.parseInt(srcIP1.getText()), Integer.parseInt(srcIP2.getText()),
-	    			Integer.parseInt(srcIP3.getText()), Integer.parseInt(srcIP4.getText()), Integer.parseInt(dstIP1.getText()),
-	    			Integer.parseInt(dstIP2.getText()), Integer.parseInt(dstIP3.getText()), Integer.parseInt(dstIP4.getText()),
-	    			Integer.parseInt(port.getText()));
-	    	
+    	{	    	          
+                //clear error message
+                message = "";
+                statusBar.setText(message);
+                
+                try {
+
+                    message = "Please enter a valid Source IP Address";
+                    statusBar.setForeground(Color.RED);
+
+                    //convert entered source IP address text to ints
+                    validSrcIP1 = Integer.parseInt(srcIP1.getText());
+                    validSrcIP2 = Integer.parseInt(srcIP2.getText());
+                    validSrcIP3 = Integer.parseInt(srcIP3.getText());
+                    validSrcIP4 = Integer.parseInt(srcIP4.getText());
+
+                    //display error message if entered source IP address is not valid
+                    if (validSrcIP1 < 0 || validSrcIP1 > 255 || validSrcIP2 < 0 || validSrcIP2 > 255 || validSrcIP3 < 0 || validSrcIP3 > 255
+                            || validSrcIP4 < 0 || validSrcIP4 > 255){
+
+                        statusBar.setText(message);
+                        return;
+                    }
+                }
+                //display error message if entered source IP address is not numeric
+                catch (NumberFormatException c) {
+
+                    statusBar.setText(message);		
+                    return;
+                }
+              
+                try {
+
+                    message = "Please enter a valid Destination IP Address";
+                    statusBar.setForeground(Color.RED);
+
+                    //convert entered destination IP address text to ints
+                    validDstIP1 = Integer.parseInt(dstIP1.getText());
+                    validDstIP2 = Integer.parseInt(dstIP2.getText());
+                    validDstIP3 = Integer.parseInt(dstIP3.getText());
+                    validDstIP4 = Integer.parseInt(dstIP4.getText());
+
+                    //display error message if entered destination IP address is not valid
+                    if (validDstIP1 < 0 || validDstIP1 > 255 || validDstIP2 < 0 || validDstIP2 > 255 || validDstIP3 < 0 || validDstIP3 > 255
+                            || validDstIP4 < 0 || validDstIP4 > 255){
+
+                        statusBar.setText(message);
+                        return;
+                    }
+                }
+                //display error message if entered destination IP address is not numeric
+                catch (NumberFormatException c) {
+
+                    statusBar.setText(message);		
+                    return;
+                }
+                
+                try {
+
+                    message = "Please enter a port number between 0 and 65535";
+                    statusBar.setForeground(Color.RED);
+                    
+                    //convert entered port text to an int
+                    validPort = Integer.parseInt(port.getText());
+                   
+                    //display error message if entered port is not valid
+                    if (validPort < 0 || validPort > 65535){
+
+                        statusBar.setText(message);
+                        return;
+                    }
+                }
+                //display error message if entered port is not numeric
+                catch (NumberFormatException c) {
+
+                    statusBar.setText(message);		
+                    return;
+                }
+                
+    		// Create the original packet. 
+	    	RdosPacket originalPacket = new RdosPacket(validSrcIP1, validSrcIP2,
+	    			validSrcIP3, validSrcIP4, validDstIP1,
+	    			validDstIP2, validDstIP3, validDstIP4,
+	    			validPort);
+
 	    	// Create a PacketTransmitter and send the original packet.
 	    	PacketTransmitter transmit = new PacketTransmitter();
 	    	transmit.send(originalPacket);
-	    	
+
 	    	// Get the received packet
 	    	RdosPacket receivedPacket = transmit.receive();
-	    	
+
 	    	// Create an analysis object
 	    	Analysis analysis = new Analysis(receivedPacket.getPacketSize(), originalPacket.getPacketSize());
 	    	int percentage = analysis.getRatio();
 //	    	System.out.println("Received size: " + receivedPacket.getPacketSize());	    	
 //	    	System.out.println("Original size: " + originalPacket.getPacketSize());
-	    	
+
 	    	message = "Received Packet to Original Packet Ratio is " + 
 	    			Integer.toString(percentage) + "%";
 			statusBar.setText(message);
 			statusBar.setForeground(Color.GREEN);
-    	}    	
+    	}
+        
     }
     
 
