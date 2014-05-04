561b12fbfa8c00b436ff8ebe140c2cd24951adc5
dandruff39 <luodanli@gmail.com>
2014-05-04 12:42:19 +0300

    Added comments to bat

--- src/netInfo.bat
+++ src/netInfo.bat
@@ -1,5 +1,10 @@
+mode con: cols=90 lines=50
+@echo *******************This is your Gateway IP Address*******************
+@echo.
 ipconfig | findstr /i "Gateway"
-
+@echo.
+@echo ******Find the Physical Address for your Gateway in this table******
+@echo.
 arp -a
 
 pause
